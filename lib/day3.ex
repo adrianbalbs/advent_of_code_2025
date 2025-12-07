d3d29b92067cddd70db7a873c9ab45523df7ce67
Adrian Balbalosa <adrianbalbs@pm.me>
2025-12-07 13:18:57 +1100

    day 3 done

--- lib/day3.ex
+++ lib/day3.ex
@@ -1,16 +1,25 @@
 defmodule Day3 do
   def main() do
-    File.stream!("lib/inputs/day3.txt")
-    |> Enum.map(&String.trim/1)
-    |> Enum.map(&part1/1)
-    |> Enum.sum()
+    p1 =
+      File.stream!("lib/inputs/day3.txt")
+      |> Enum.map(&String.trim/1)
+      |> Enum.map(&String.graphemes/1)
+      |> Enum.map(&part1/1)
+      |> Enum.sum()
+
+    IO.puts(p1)
+
+    p2 =
+      File.stream!("lib/inputs/day3.txt")
+      |> Enum.map(&String.trim/1)
+      |> Enum.map(&String.graphemes/1)
+      |> Enum.map(&part2/1)
+      |> Enum.sum()
+
+    IO.puts(p2)
   end
 
-  def part1(line) do
-    nums =
-      line
-      |> String.graphemes()
-
+  def part1(nums) do
     # if the largest element for the first part is at the end, then we cannot form a two-digit number
     prefix_nums = List.delete_at(nums, length(nums) - 1)
 
@@ -22,7 +31,31 @@ defmodule Day3 do
 
     # get the list after the earliest occurence of that element, so we can then find the maximum elem to use
     {_prefix, [_x | suffix]} = Enum.split_while(nums, fn x -> x != elem end)
-    largest = Enum.max(suffix)
-    String.to_integer(elem <> largest)
+    String.to_integer(elem <> Enum.max(suffix))
+  end
+
+  def part2(nums) do
+    String.to_integer(part2_helper(nums, 11))
+  end
+
+  defp part2_helper(nums, n) do
+    # looks like it'll be a similar concept, but recursive
+    # needs is at least n - 11, then n - 10, then n - 9 and so on
+    prefix_nums = Enum.slice(nums, 0, length(nums) - n)
+    # get the first element which matches the prefix
+    elem =
+      Enum.to_list(9..1//-1)
+      |> Enum.find(fn x -> Integer.to_string(x) in prefix_nums end)
+      |> Integer.to_string()
+
+    {_prefix, [_x | suffix]} = Enum.split_while(nums, fn x -> x != elem end)
+
+    # we need 1 more integer to fill, so use the previous logic
+    if n == 1 do
+      elem <> Enum.max(suffix)
+    else
+      elem <> part2_helper(suffix, n - 1)
+    end
   end
 end
+
--- notes.md
+++ notes.md
@@ -9,3 +9,8 @@
 - if we find the earliest occurence, it gives us a larger window for finding the largest elem
 - iterate backwards from 9-1 for list of size n - 1 and find largest in that list
   - Then split on the earliest occurence of that integer, and from the suffix, obtain the maximum elem to use
+## Part 2
+- I think we can do this recursively, and re-use the logic from part 1 as a base case for when n = 1
+- For selecting the prefix, we must ensure that the list size for the prefix list is length(nums) - n
+  where n is from 11 down to 1, otherwise, in the suffix, we won't have enough elements to pick from to form a full number
+
